41e536047fb02f1f145afd723bb66baf7e9b3aeb
Uelisson <tagots09@gmail.com>
2025-04-01 13:39:20 -0300

    Criando repositorio

---
+++ src/test/java/com/uelissonsantos/workshopMongoDb/WorkshopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uelissonsantos.workshopMongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
